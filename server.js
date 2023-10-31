f623150895353b88e20e2a0cd2c59fcebd34728c
kevindimayuga <k.dimayuga22@gmail.com>
2023-10-31 16:30:40 -0500

    set up variables for server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // import all necessary packages, files, etc.
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -9,3 +8,10 @@ const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// This will create the express app and set up the port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// This will set up the handlebars engine with custom helpers
+const hbs = exphbs.create({ helpers });
